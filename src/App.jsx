4ba8a639ea22ee0759ab0a6d6dc7553258d3afdf
NiravLathiya <nv.globaliasoft@gmail.com>
2020-09-25 12:55:13 +0530

    added styling

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import HomePage from './components/Pages/Home';
+
+
+function App() {
+   return <>
+      <HomePage />
+   </>;
+}
+
+export default App;
---
+++ src/components/Pages/Home/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './assets/style.scss'
+import Directory from '../../Elements/Directory'
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <Directory />
+        </div>
+    )
+}
+
+export default HomePage
